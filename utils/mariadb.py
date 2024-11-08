d89f13e2957a590e5331cbae0324c9f60f008bc0
DjBuRnOuT <1522047+DjBuRnOuT@users.noreply.github.com>
2024-11-08 17:32:35 -0500

    trying to fix

--- utils/mariadb.py
+++ utils/mariadb.py
@@ -1,10 +1,11 @@
 from base_logger import logger
 import httpx
+import urllib.parse
 
 # Max number of versions to be fetched in each version branch
 MAX_ALLOWED_VERSIONS = 3
 VERSIONS_API = "https://downloads.mariadb.org/rest-api/mariadb/"
-VERSION_RESOURCE_API = f"https://downloads.mariadb.org/rest-api/mariadb/{{version}}/"
+VERSION_RESOURCE_API = "https://downloads.mariadb.org/rest-api/mariadb/{version}/"
 oneinstack_compatible_versions = ["5.5", "10.5", "10.4", "10.11"]
 
 
@@ -12,21 +13,22 @@ def make_cache() -> tuple[list[dict[str, str]], list[dict[str, str]]]:
     return_list = []
     latest_meta = []
     live_version_branches = [b["release_id"] for b in httpx.get(VERSIONS_API).json()["major_releases"]
-                             if b["release_status"] == "Stable"]
+                            if b["release_status"] == "Stable"]
     version_branches = list(set(oneinstack_compatible_versions + live_version_branches))
     version_branches.sort(reverse=True)
     logger.info(f"Available MariaDB versions: {version_branches}")
     for version in version_branches:
+        encoded_version = urllib.parse.quote(version)
         this_branch_latest_meta = {
             "version_file_name": f"mariadb{version.replace(".", "")}_ver",
             "version": None
         }
         this_version_packages = []
-        patch_list = list(httpx.get(VERSION_RESOURCE_API.format(version=version)).json()["releases"].values())
+        patch_list = list(httpx.get(VERSION_RESOURCE_API.format(version=encoded_version)).json()["releases"].values())
         for patch in patch_list:
             try:
                 linux_package = [f for f in patch["files"] if f["os"] == "Linux" and f["cpu"].lower() == "x86_64"
-                                 and "linux-systemd" in f["file_name"]][0]
+                                and "linux-systemd" in f["file_name"]][0]
                 linux_package_info = {
                     "file_name": linux_package["file_name"],
                     "url": linux_package["file_download_url"],
